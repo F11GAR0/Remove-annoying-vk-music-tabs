1b752d03d710bb90f84dfb418ba91002b704a08e
Александр <den180777@mail.ru>
2022-11-04 18:55:16 +0300

    initial commit

---
+++ remove annoying vk music.js
@@ -0,0 +1,6 @@
+//реклама ебучая
+document.getElementsByClassName('CatalogSection CatalogSection--header_section')[0].hidden = true;
+//мусор
+document.getElementsByClassName('CatalogBlock CatalogBlock--divided')[0].hidden = true;
+document.getElementsByClassName('CatalogBlock CatalogBlock--divided')[2].hidden = true;
+document.getElementsByClassName('CatalogBlock CatalogBlock--divided')[3].hidden = true;
